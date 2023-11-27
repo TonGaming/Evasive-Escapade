8eac56945aa66ada5b00b964453c104f9cb11ffc
LadyKillerr <dungvuduc0509@gmail.com>
2023-11-27 18:12:41 +0700

    feat: added a timer for falling blocks

---
+++ Assets/Scripts/Obstacles Timing.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesTiming : MonoBehaviour
+{
+
+    //[SerializeField] int spawnTimeVariance = 1;
+    //[SerializeField] int spawnTimeMinimum = 4;
+    //[SerializeField] int spawnTimeMaximum = 7;
+
+    [SerializeField] float delayTime;
+
+    Rigidbody cubeBombRigidbody;
+    
+    
+
+    private void Awake()
+    {
+        cubeBombRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Time.time > delayTime)
+        {
+            cubeBombRigidbody.mass = 1;
+
+            Invoke("ResetFallingBlocks", 5);
+
+        }
+    }
+
+    void ResetFallingBlocks()
+    {
+        Destroy(gameObject);
+    }
+
+    //void FallingBlocksTimer()
+    //{
+    //    Invoke(SpawnFallingBlocks(), GetRandomSpawnTime());
+    //}
+
+    //void SpawnFallingBlocks()
+    //{
+    //    Instantiate();
+    //}
+
+    //int GetRandomSpawnTime()
+    //{
+    //    // tạo một biến spawnTime Random trong khoảng định trước
+    //    int spawnTime = Random.Range(spawnTimeMinimum - spawnTimeVariance, spawnTimeMaximum + spawnTimeVariance);
+
+
+    //    // clamp các giá trị âm
+    //    return Mathf.Clamp(spawnTime, 0, int.MaxValue);
+    //}
+}
